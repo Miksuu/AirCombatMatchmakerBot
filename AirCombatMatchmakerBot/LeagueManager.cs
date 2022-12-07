5a7fc60c3ee394889738f1b95d93f880a0b425da
Miksuu <imiksuu96@gmail.com>
2022-12-07 17:40:27 +0200

    Split League creation in to methods

--- AirCombatMatchmakerBot/LeagueManager.cs
+++ AirCombatMatchmakerBot/LeagueManager.cs
@@ -1,4 +1,6 @@
 ﻿using Discord;
+using System.Formats.Asn1;
+using System.Threading.Channels;
 
 public static class LeagueManager
 {
@@ -18,51 +20,69 @@ public static class LeagueManager
 
                 foreach (LeagueName leagueName in Enum.GetValues(typeof(LeagueName)))
                 {
-                    Log.WriteLine("Looping on leagueName: " + leagueName.ToString(), LogLevel.VERBOSE);
-
-                    ILeague leagueInterface = MakeInterfaceFromAEnumName(leagueName);
-
-                    Log.WriteLine("Made a " + nameof(leagueInterface) + " named: "+
-                        leagueInterface.LeagueName,LogLevel.VERBOSE );
-
-                    string leagueEnumAttrValue = 
-                        ClassExtensions.GetEnumMemberAttrValue(leagueInterface.LeagueName);
-
-                    Log.WriteLine(nameof(leagueEnumAttrValue) + ": " +
-                        leagueEnumAttrValue,LogLevel.VERBOSE);
-
-                    string leagueButtonRegisterationCustomId = 
-                        leagueInterface.LeagueName.ToString() + "_register";
-
-                    Log.WriteLine(nameof(leagueButtonRegisterationCustomId) + ": " +
-                        leagueButtonRegisterationCustomId, LogLevel.VERBOSE);
-
-                    leagueInterface.LeagueData = new();
-
-                    leagueInterface.LeagueData.leagueChannelMessageId =
-                        await BotMessaging.CreateButton(
-                            channel,
-                            "Click this button to register to: " + leagueEnumAttrValue,
-                            "Join",
-                            leagueButtonRegisterationCustomId); // Maybe replace this with some other system
-
-                    if (leagueInterface.LeagueData.leagueChannelMessageId != 0)
-                    {
-                        if (Database.Instance.StoredLeagues != null)
-                        {
-                            //Database.Instance.StoredLeagues.Add(leagueInterface);
-                        }
-                        else Log.WriteLine(nameof(Database.Instance.StoredLeagues) +
-                            " was null!", LogLevel.CRITICAL);
-                    }
-                    else Log.WriteLine(nameof(leagueInterface.LeagueData.leagueChannelMessageId) +
-                        " was 0, channel not created succesfully?", LogLevel.CRITICAL);
+                    CreateALeague(channel, leagueName);
                 }
             }
             else Log.WriteLine("Channel was null, wrong id in the code?", LogLevel.CRITICAL);
 
 
         } else Exceptions.BotGuildRefNull();
+    }
+
+    public static void CreateALeague(ITextChannel _channel, LeagueName _leagueName)
+    {
+        Log.WriteLine("Looping on leagueName: " + _leagueName.ToString(), LogLevel.VERBOSE);
+
+        ILeague leagueInterface = MakeInterfaceFromAEnumName(_leagueName);
+
+        Log.WriteLine("Made a " + nameof(leagueInterface) + " named: " +
+            leagueInterface.LeagueName, LogLevel.VERBOSE);
+
+        leagueInterface = CreateALeagueJoinButton(_channel, leagueInterface).Result;
+
+        StoreTheLeague(leagueInterface);
+    }
+
+    public static async Task<ILeague> CreateALeagueJoinButton(
+        ITextChannel _channel, ILeague _leagueInterface)
+    {
+        string leagueEnumAttrValue =
+            ClassExtensions.GetEnumMemberAttrValue(_leagueInterface.LeagueName);
+
+        Log.WriteLine(nameof(leagueEnumAttrValue) + ": " +
+            leagueEnumAttrValue, LogLevel.VERBOSE);
+
+        string leagueButtonRegisterationCustomId =
+            _leagueInterface.LeagueName.ToString() + "_register";
+
+        Log.WriteLine(nameof(leagueButtonRegisterationCustomId) + ": " +
+            leagueButtonRegisterationCustomId, LogLevel.VERBOSE);
+
+        _leagueInterface.LeagueData = new();
+
+        _leagueInterface.LeagueData.leagueChannelMessageId =
+            await BotMessaging.CreateButton(
+                _channel,
+                "Click this button to register to: " + leagueEnumAttrValue,
+                "Join",
+                leagueButtonRegisterationCustomId); // Maybe replace this with some other system
+
+        return _leagueInterface;
+    }
+
+    public static void StoreTheLeague(ILeague _leagueInterface)
+    {
+        if (_leagueInterface.LeagueData.leagueChannelMessageId != 0)
+        {
+            if (Database.Instance.StoredLeagues != null)
+            {
+                //Database.Instance.StoredLeagues.Add(_leagueInterface);
+            }
+            else Log.WriteLine(nameof(Database.Instance.StoredLeagues) +
+                " was null!", LogLevel.CRITICAL);
+        }
+        else Log.WriteLine(nameof(_leagueInterface.LeagueData.leagueChannelMessageId) +
+            " was 0, channel not created succesfully?", LogLevel.CRITICAL);
     }
 
     public static ILeague MakeInterfaceFromAEnumName<T> (T _enumInput)
