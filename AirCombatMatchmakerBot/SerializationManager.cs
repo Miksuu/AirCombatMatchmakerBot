e1157a3b3e87f01a6c651cab8396265804228324
Miksuu <imiksuu96@gmail.com>
2022-12-07 18:29:02 +0200

    Fix Serialization faults

--- AirCombatMatchmakerBot/SerializationManager.cs
+++ AirCombatMatchmakerBot/SerializationManager.cs
@@ -6,7 +6,6 @@ public static class SerializationManager
 {
     static string dbPath = @"C:\AirCombatMatchmakerBot\Data\database.json";
 
-    // Run this everytime when relevant data needs to be saved to the database
     public static async Task SerializeDB(bool _circularDependency = false)
     {
         Log.WriteLine("SERIALIZING DB", LogLevel.VERBOSE);
@@ -15,20 +14,16 @@ public static class SerializationManager
         {
             await SerializeUsersOnTheServer();
         }
-        
+
         Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
-        //serializer.Converters.Add(new Newtonsoft.Json.Converters.JavaScriptDateTimeConverter());
-        //serializer.NullValueHandling = Newtonsoft.Json.NullValueHandling.Include;
+        serializer.Converters.Add(new Newtonsoft.Json.Converters.JavaScriptDateTimeConverter());
+        serializer.NullValueHandling = Newtonsoft.Json.NullValueHandling.Include;
         serializer.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.All;
         serializer.Formatting = Newtonsoft.Json.Formatting.Indented;
-        serializer.MaxDepth = 64;
-
-        //Console.WriteLine("SERIALIZATION, STARTING for " + Database.Instance.Tournaments.Count);
 
         using (StreamWriter sw = new StreamWriter(dbPath))
         using (Newtonsoft.Json.JsonWriter writer = new Newtonsoft.Json.JsonTextWriter(sw))
         {
-            //string jsonStringSer = JsonSerializer.Serialize<Database>(Database.Instance, options);
             serializer.Serialize(writer, Database.Instance, typeof(Database));
             writer.Close();
             sw.Close();
@@ -77,24 +72,24 @@ public static class SerializationManager
         Log.WriteLine("User serialization done on the server", LogLevel.VERBOSE);
     }
 
-    public static Task DeSerializeDB()
+
+
+    public static async Task DeSerializeDB()
     {
-        string jsonString = "";
-
-        using (var fs = File.Open(dbPath, FileMode.Open, FileAccess.Read))
+        string json = File.ReadAllText(dbPath);
+        if (json != "0")
         {
-            using (StreamReader r = new StreamReader(fs))
-            {
-                jsonString = r.ReadToEnd();
-                Database.Instance = JsonConvert.DeserializeObject<Database>(jsonString);
-                r.Close();
-            }
-            fs.Close();
+            Database.Instance = Newtonsoft.Json.JsonConvert.DeserializeObject<Database>(json,
+                new Newtonsoft.Json.JsonSerializerSettings
+                {
+                    TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto,
+                    NullValueHandling = Newtonsoft.Json.NullValueHandling.Include,
+                });
         }
-
-        Log.WriteLine("DE-SERIALIZING JSON: " + jsonString, LogLevel.VERBOSE);
-        
-        return Task.CompletedTask;
+        else
+        {
+            Database.Instance = new();
+        }
     }
 
     public static void AddUserIdToCachedList(string _userString, ulong _userId)
