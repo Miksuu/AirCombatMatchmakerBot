00d3441babab653b2923062c31f47bb635fe8c1a
Miksuu <imiksuu96@gmail.com>
2022-12-27 19:47:43 +0200

    Make proper GetSetters for BaseLeague

--- AirCombatMatchmakerBot/Data/Categories/LeagueCategories/BaseLeague.cs
+++ AirCombatMatchmakerBot/Data/Categories/LeagueCategories/BaseLeague.cs
@@ -7,50 +7,100 @@ public abstract class BaseLeague : ILeague
 {
     CategoryName ILeague.LeagueCategoryName
     {
-        get => leagueCategoryName;
-        set => leagueCategoryName = value;
+        get
+        {
+            Log.WriteLine("Getting " + nameof(leagueCategoryName) + ": " + leagueCategoryName, LogLevel.VERBOSE);
+            return leagueCategoryName;
+        }
+        set
+        {
+            Log.WriteLine("Setting " + nameof(leagueCategoryName) + leagueCategoryName
+                + " to: " + value, LogLevel.VERBOSE);
+            leagueCategoryName = value;
+        }
     }
 
     Era ILeague.LeagueEra
     {
-        get => leagueEra;
-        set => leagueEra = value;
+        get
+        {
+            Log.WriteLine("Getting " + nameof(leagueEra) + ": " + leagueEra, LogLevel.VERBOSE);
+            return leagueEra;
+        }
+        set
+        {
+            Log.WriteLine("Setting " + nameof(leagueEra) + leagueEra
+                + " to: " + value, LogLevel.VERBOSE);
+            leagueEra = value;
+        }
     }
 
     int ILeague.LeaguePlayerCountPerTeam
     {
-        get => leaguePlayerCountPerTeam;
-        set => leaguePlayerCountPerTeam = value;
+        get
+        {
+            Log.WriteLine("Getting " + nameof(leaguePlayerCountPerTeam) + ": " + leaguePlayerCountPerTeam, LogLevel.VERBOSE);
+            return leaguePlayerCountPerTeam;
+        }
+        set
+        {
+            Log.WriteLine("Setting " + nameof(leaguePlayerCountPerTeam) + leaguePlayerCountPerTeam
+                + " to: " + value, LogLevel.VERBOSE);
+            leaguePlayerCountPerTeam = value;
+        }
     }
 
     List<UnitName> ILeague.LeagueUnits
     {
-        get => leagueUnits;
-        set => leagueUnits = value;
+        get
+        {
+            Log.WriteLine("Getting " + nameof(leagueUnits) + ": " + leagueUnits, LogLevel.VERBOSE);
+            return leagueUnits;
+        }
+        set
+        {
+            Log.WriteLine("Setting " + nameof(leagueUnits) + leagueUnits
+                + " to: " + value, LogLevel.VERBOSE);
+            leagueUnits = value;
+        }
     }
 
     LeagueData ILeague.LeagueData
     {
-        get => leagueData;
-        set => leagueData = value;
+        get
+        {
+            Log.WriteLine("Getting " + nameof(leagueData) + ": " + leagueData, LogLevel.VERBOSE);
+            return leagueData;
+        }
+        set
+        {
+            Log.WriteLine("Setting " + nameof(leagueData) + leagueData
+                + " to: " + value, LogLevel.VERBOSE);
+            leagueData = value;
+        }
     }
 
     DiscordLeagueReferences ILeague.DiscordLeagueReferences
     {
-        get => discordleagueReferences;
-        set => discordleagueReferences = value;
+        get
+        {
+            Log.WriteLine("Getting " + nameof(discordleagueReferences) + ": " + discordleagueReferences, LogLevel.VERBOSE);
+            return discordleagueReferences;
+        }
+        set
+        {
+            Log.WriteLine("Setting " + nameof(discordleagueReferences) + discordleagueReferences
+                + " to: " + value, LogLevel.VERBOSE);
+            discordleagueReferences = value;
+        }
     }
 
-    [DataMember] protected CategoryName leagueCategoryName;
-
     // Generated based on the implementation
+    [DataMember] protected CategoryName leagueCategoryName;
     [DataMember] protected Era leagueEra;
     [DataMember] protected int leaguePlayerCountPerTeam;
-
     [DataMember] protected List<UnitName> leagueUnits = new List<UnitName>();
-
     [DataMember] protected LeagueData leagueData = new LeagueData();
-
     [DataMember] protected DiscordLeagueReferences discordleagueReferences = new DiscordLeagueReferences();
 
     public BaseLeague()
@@ -75,7 +125,7 @@ public abstract class BaseLeague : ILeague
         }
 
         Log.WriteLine("Team not found! Admin trying to access challenge" +
-    " of a league that he's not registered to?", LogLevel.WARNING);
+            " of a league that he's not registered to?", LogLevel.WARNING);
 
         return null;
     }
