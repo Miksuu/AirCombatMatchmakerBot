fc40ce59b6ecaddb08bac3520910ca4119e431b3
Miksuu <imiksuu96@gmail.com>
2023-04-13 14:16:00 +0300

    Work in progress add match confirm button

---
+++ AirCombatMatchmakerBot/Data/Buttons/Implementations/CONFIRMMATCHENTRYBUTTON.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using System.Data;
+using System;
+using System.Runtime.Serialization;
+using Discord.WebSocket;
+using System.Threading.Channels;
+
+[DataContract]
+public class CONFIRMMATCHENTRYBUTTON : BaseButton
+{
+    public CONFIRMMATCHENTRYBUTTON()
+    {
+        buttonName = ButtonName.CONFIRMMATCHENTRYBUTTON;
+        buttonLabel = "CONFIRM";
+        buttonStyle = ButtonStyle.Success;
+        ephemeralResponse = true;
+    }
+
+    public override Task<(string, bool)> ActivateButtonFunction(
+        SocketMessageComponent _component, InterfaceMessage _interfaceMessage)
+    {
+        return Task.FromResult(("Not implemented yet!", false));
+    }
+}
---
+++ AirCombatMatchmakerBot/Data/Messages/Implementations/CONFIRMMATCHENTRYMESSAGE.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using System.Data;
+using System;
+using System.Runtime.Serialization;
+using Discord.WebSocket;
+using System.Threading.Channels;
+using System.Reflection;
+using System.Collections.Concurrent;
+
+[DataContract]
+public class CONFIRMMATCHENTRYMESSAGE : BaseMessage
+{
+    public CONFIRMMATCHENTRYMESSAGE()
+    {
+        messageName = MessageName.CONFIRMMATCHENTRYMESSAGE;
+
+        messageButtonNamesWithAmount = new ConcurrentDictionary<ButtonName, int>(
+            new ConcurrentBag<KeyValuePair<ButtonName, int>>()
+            {
+                new KeyValuePair<ButtonName, int>(ButtonName.CONFIRMMATCHENTRYBUTTON, 1),
+            });
+
+        messageEmbedTitle = "This message confirms the match entry [add more detailed message here]";
+    }
+
+    public override string GenerateMessage()
+    {
+        return messageDescription;
+    }
+}
