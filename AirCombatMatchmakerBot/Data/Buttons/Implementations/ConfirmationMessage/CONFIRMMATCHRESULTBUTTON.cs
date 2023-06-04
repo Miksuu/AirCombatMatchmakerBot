70d47a0b6b7c2cbcc92a7db560aca982bbb72a22
Miksuu <imiksuu96@gmail.com>
2023-06-04 13:14:20 +0300

    Insert trycatch to the the confirmamatchresultbutton

--- AirCombatMatchmakerBot/Data/Buttons/Implementations/ConfirmationMessage/CONFIRMMATCHRESULTBUTTON.cs
+++ AirCombatMatchmakerBot/Data/Buttons/Implementations/ConfirmationMessage/CONFIRMMATCHRESULTBUTTON.cs
@@ -22,73 +22,64 @@ public class CONFIRMMATCHRESULTBUTTON : BaseButton
     public override async Task<Response> ActivateButtonFunction(
         SocketMessageComponent _component, InterfaceMessage _interfaceMessage)
     {
-        mcc = new MatchChannelComponents(_interfaceMessage);
-        if (mcc.interfaceLeagueCached == null || mcc.leagueMatchCached == null)
-        {
-            string errorMsg = nameof(mcc.interfaceLeagueCached) + " or " +
-                nameof(mcc.leagueMatchCached) + " was null!";
-            Log.WriteLine(errorMsg, LogLevel.CRITICAL);
-            return new Response(errorMsg, false);
-        }
-
-        string finalResponse = string.Empty;
-
-        ulong componentPlayerId = _component.User.Id;
-
-        Log.WriteLine("Activating button function: " + buttonName.ToString() + " by: " +
-            componentPlayerId + " in msg: " + _interfaceMessage.MessageId, LogLevel.VERBOSE);
-
-        List<ReportData> reportDataTupleWithString;
-
         try
         {
+            mcc = new MatchChannelComponents(_interfaceMessage);
+            if (mcc.interfaceLeagueCached == null || mcc.leagueMatchCached == null)
+            {
+                string errorMsg = nameof(mcc.interfaceLeagueCached) + " or " +
+                    nameof(mcc.leagueMatchCached) + " was null!";
+                Log.WriteLine(errorMsg, LogLevel.CRITICAL);
+                return new Response(errorMsg, false);
+            }
+
+            string finalResponse = string.Empty;
+
+            ulong componentPlayerId = _component.User.Id;
+
+            Log.WriteLine("Activating button function: " + buttonName.ToString() + " by: " +
+                componentPlayerId + " in msg: " + _interfaceMessage.MessageId, LogLevel.VERBOSE);
+
+            List<ReportData> reportDataTupleWithString;
+
             reportDataTupleWithString =
                 mcc.leagueMatchCached.MatchReporting.GetTeamReportDatasOfTheMatchWithPlayerId(
                     mcc.interfaceLeagueCached, mcc.leagueMatchCached, componentPlayerId);
-        }
-        catch (Exception ex)
-        {
-            return new Response(ex.Message, false);
-        }
 
-        if (reportDataTupleWithString.ElementAt(0).ConfirmedMatch)
-        {
-            return new Response("You have already confirmed the match!", false);
-        }
+            if (reportDataTupleWithString.ElementAt(0).ConfirmedMatch)
+            {
+                return new Response("You have already confirmed the match!", false);
+            }
 
-        reportDataTupleWithString.ElementAt(0).ConfirmedMatch = true;
+            reportDataTupleWithString.ElementAt(0).ConfirmedMatch = true;
 
-        if (reportDataTupleWithString.ElementAt(1).ConfirmedMatch == true)
-        {
-            //mcc.leagueMatchCached.MatchReporting.MatchState = MatchState.MATCHDONE;
-            Log.WriteLine("Both teams are done with the reporting on match: " +
-                mcc.leagueMatchCached.MatchId, LogLevel.DEBUG);
-        }
+            if (reportDataTupleWithString.ElementAt(1).ConfirmedMatch == true)
+            {
+                Log.WriteLine("Both teams are done with the reporting on match: " +
+                    mcc.leagueMatchCached.MatchId, LogLevel.DEBUG);
+            }
+
+            InterfaceMessage confirmationMessage =
+                    Database.Instance.Categories.FindInterfaceCategoryWithId(
+                        _interfaceMessage.MessageCategoryId).FindInterfaceChannelWithIdInTheCategory(
+                            _interfaceMessage.MessageChannelId).FindInterfaceMessageWithNameInTheChannel(
+                                MessageName.CONFIRMATIONMESSAGE);
 
 
-        InterfaceMessage confirmationMessage;
-        try
-        {
-            confirmationMessage =
-                Database.Instance.Categories.FindInterfaceCategoryWithId(
-                    _interfaceMessage.MessageCategoryId).FindInterfaceChannelWithIdInTheCategory(
-                        _interfaceMessage.MessageChannelId).FindInterfaceMessageWithNameInTheChannel(
-                            MessageName.CONFIRMATIONMESSAGE);
+            Log.WriteLine("Found: " + confirmationMessage.MessageId + " with content: " +
+                confirmationMessage.MessageDescription, LogLevel.DEBUG);
+
+            await confirmationMessage.GenerateAndModifyTheMessage();
+
+            Log.WriteLine("Reached end before the return with player id: " + componentPlayerId +
+                " with finalResposne: " + finalResponse, LogLevel.DEBUG);
+
+            return new Response(finalResponse, true);
         }
         catch (Exception ex)
         {
             Log.WriteLine(ex.Message, LogLevel.CRITICAL);
             return new Response(ex.Message, false);
         }
-
-        Log.WriteLine("Found: " + confirmationMessage.MessageId + " with content: " + 
-            confirmationMessage.MessageDescription, LogLevel.DEBUG);
-
-        await confirmationMessage.GenerateAndModifyTheMessage();
-
-        Log.WriteLine("Reached end before the return with player id: " + componentPlayerId +
-            " with finalResposne: " + finalResponse, LogLevel.DEBUG);
-
-        return new Response(finalResponse, true);
     }
 }
