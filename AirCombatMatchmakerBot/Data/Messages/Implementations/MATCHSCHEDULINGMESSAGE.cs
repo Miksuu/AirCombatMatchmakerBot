196d69128d7c664d36bb2693b4b0920cfc5b84c8
Miksuu <imiksuu96@gmail.com>
2023-06-23 21:46:44 +0300

    Return the original Message description on the MatchSchedulingInstructionsMessage

--- AirCombatMatchmakerBot/Data/Messages/Implementations/MATCHSCHEDULINGMESSAGE.cs
+++ AirCombatMatchmakerBot/Data/Messages/Implementations/MATCHSCHEDULINGMESSAGE.cs
@@ -39,40 +39,6 @@ public class MATCHSCHEDULINGMESSAGE : BaseMessage
 
     public override string GenerateMessage()
     {
-        try
-        {
-            mcc = new MatchChannelComponents(this);
-            if (mcc.interfaceLeagueCached == null || mcc.leagueMatchCached == null)
-            {
-                string errorMsg = nameof(mcc.interfaceLeagueCached) + " or " +
-                    nameof(mcc.leagueMatchCached) + " was null!";
-                Log.WriteLine(errorMsg, LogLevel.CRITICAL);
-                return errorMsg;
-            }
-
-            var scheduleObject = mcc.leagueMatchCached.ScheduleObject;
-            var teamsInTheMatch = mcc.leagueMatchCached.TeamsInTheMatch;
-            if (!teamsInTheMatch.ContainsKey(scheduleObject.TeamIdThatRequestedScheduling))
-            {
-                return thisInterfaceMessage.MessageDescription;
-            }
-
-            var requestedTime = TimeService.ConvertToDateTimeFromUnixTime(scheduleObject.RequestedSchedulingTimeInUnixTime);
-
-            var teamNameThatScheduled = teamsInTheMatch.First(
-                t => t.Key == scheduleObject.TeamIdThatRequestedScheduling).Value;
-            /*
-            string time = TimeService.ReturnTimeLeftAsStringFromTheTimeTheActionWillTakePlace(requestedTime);
-            Log.WriteLine("time: " + time, LogLevel.VERBOSE);
-            Log.WriteLine("teamNameThatScheduled: " + teamNameThatScheduled, LogLevel.VERBOSE);*/
-
-            return requestedTime + " requested by team: " + teamNameThatScheduled;
-        }
-        catch (Exception ex) 
-        {
-            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
-            return ex.Message;
-        }
-
+        return thisInterfaceMessage.MessageDescription;
     }
 }
