c79b205916ca7ffed8738ca1946099233b0cf336
Miksuu <imiksuu96@gmail.com>
2023-06-23 21:50:17 +0300

    Modify the scheduling message instructions to include the hours and minutes

--- AirCombatMatchmakerBot/Data/Messages/Implementations/MATCHSCHEDULINGMESSAGE.cs
+++ AirCombatMatchmakerBot/Data/Messages/Implementations/MATCHSCHEDULINGMESSAGE.cs
@@ -19,8 +19,9 @@ public class MATCHSCHEDULINGMESSAGE : BaseMessage
         thisInterfaceMessage.MessageEmbedTitle = "Schedule your match here using the /schedule command";
         thisInterfaceMessage.MessageDescription = "Enter the time you would be able to play the match on in format: \n " +
             "``/schedule 27.11.2022 1030z``\n" + "Instead of using the date 27.11.2022 you can use: [today, tomorrow]," +
-            " [any weekday] (will assume the next day that's available), [now] (will schedule the match 20 minutes away, 5 minutes time to accept)." +
-            "\nYou can also use slashes in the date and put the time first, for example: ``/schedule 11z 27/11/2022``\n\n" +
+            " [any weekday] (will assume the next day that's available), [now] (will schedule the match 20 minutes away, 5 minutes time to accept)," +
+            "[x hours, minutes] (schedules the match from x hours and minutes away)\n" +
+            "You can also use slashes in the date and put the time first, for example: ``/schedule 11z 27/11/2022``\n\n" +
             "Examples:\n" +
             "``/schedule 0659z 01.02.2024``\n" +
             "``/schedule 03/04/2024 07z``\n" +
@@ -29,6 +30,9 @@ public class MATCHSCHEDULINGMESSAGE : BaseMessage
             "``/schedule today 1920z``\n" +
             "``/schedule tomorrow 2023z``\n" +
             "``/schedule now``\n\n" + 
+            "``/schedule 6 hours``\n\n" + 
+            "``/schedule 4 hours 30minutes``\n\n" + 
+            "``/schedule 42 minutes``\n\n" + 
             "Once you agree to a certain time, use ``/schedule accept`` command, or click the button below!";
     }
 
