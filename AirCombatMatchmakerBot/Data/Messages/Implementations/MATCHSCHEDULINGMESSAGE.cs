5a7b93fb62005f782b5c9e521b6394fb0f350c89
Miksuu <imiksuu96@gmail.com>
2023-06-23 12:10:16 +0300

    Match scheduling instructions

--- AirCombatMatchmakerBot/Data/Messages/Implementations/MATCHSCHEDULINGMESSAGE.cs
+++ AirCombatMatchmakerBot/Data/Messages/Implementations/MATCHSCHEDULINGMESSAGE.cs
@@ -15,8 +15,19 @@ public class MATCHSCHEDULINGMESSAGE : BaseMessage
             {
             });
 
-        thisInterfaceMessage.MessageEmbedTitle = "Match Scheduling";
-        thisInterfaceMessage.MessageDescription = "Schedule the match here";
+        thisInterfaceMessage.MessageEmbedTitle = "Schedule your match here using the /schedule command";
+        thisInterfaceMessage.MessageDescription = "Enter the time you would be able to play the match on in format: \n " +
+            "``/schedule 27.11.2022 1030z``\n" + "Instead of using the date 27.11.2022 you can use: [today, tomorrow]," +
+            " [any weekday] (will assume the next day that's available), [now] (will schedule the match 20 minutes away, 5 minutes time to accept)." +
+            "\nYou can also use slashes in the date and put the time first, for example: ``/schedule 11z 27/11/2022``\n\n" +
+            "Examples:\n" +
+            "``/schedule 0659z 01.02.2024``\n" +
+            "``/schedule 03/04/2024 07z``\n" +
+            "``/schedule 1835z saturday ``\n" +
+            "``/schedule sunday 19z``\n" +
+            "``/schedule today 1920z``\n" +
+            "``/schedule tomorrow 2023z``\n" +
+            "``/schedule now``\n";
     }
 
     protected override void GenerateButtons(ComponentBuilder _component, ulong _leagueCategoryId)
@@ -44,7 +55,6 @@ public class MATCHSCHEDULINGMESSAGE : BaseMessage
                 return thisInterfaceMessage.MessageDescription;
             }
 
-            
             var requestedTime = TimeService.ConvertToDateTimeFromUnixTime(scheduleObject.RequestedSchedulingTimeInUnixTime);
 
             var teamNameThatScheduled = teamsInTheMatch.First(
