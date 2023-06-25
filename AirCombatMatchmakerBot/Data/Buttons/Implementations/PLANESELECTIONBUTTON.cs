5a9bea44a8ea685e5ee169d22ac53f8d485befd7
Miksuu <imiksuu96@gmail.com>
2023-06-25 09:21:29 +0300

    Cleanup try-catch stuff on ActivateButtonFunction method

--- AirCombatMatchmakerBot/Data/Buttons/Implementations/PLANESELECTIONBUTTON.cs
+++ AirCombatMatchmakerBot/Data/Buttons/Implementations/PLANESELECTIONBUTTON.cs
@@ -26,68 +26,59 @@ public class PLANESELECTIONBUTTON : BaseButton
     public override Task<Response> ActivateButtonFunction(
         SocketMessageComponent _component, InterfaceMessage _interfaceMessage)
     {
-        Team playerTeam;
-
-        mcc = new MatchChannelComponents(_interfaceMessage);
-        if (mcc.interfaceLeagueCached == null || mcc.leagueMatchCached == null)
-        {
-            Log.WriteLine(nameof(mcc) + " was null!", LogLevel.CRITICAL);
-            return Task.FromResult(new Response(nameof(mcc) + " was null!", false));
-        }
-
-        var playerId = _component.User.Id;
-        string[] splitStrings = thisInterfaceButton.ButtonCustomId.Split('_');
-        string playerSelectedPlane = splitStrings[0];
-
-        Log.WriteLine("Starting to find team with playerId: " + playerId +
-            " with selected plane: " + playerSelectedPlane, LogLevel.VERBOSE);
-
         try
         {
-            playerTeam = mcc.interfaceLeagueCached.LeagueData.FindActiveTeamByPlayerIdInAPredefinedLeagueByPlayerId(playerId);
-        }
-        catch (Exception ex)
-        {
-            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
-            return Task.FromResult(new Response(ex.Message, false));
-        }
-
-        Log.WriteLine("Finding with " + nameof(playerTeam) + ": " + playerTeam.TeamName + " with id: " + playerTeam.TeamId +
-            " on league: " + mcc.interfaceLeagueCached.LeagueCategoryName, LogLevel.VERBOSE);
-
-        foreach (var teamKvp in mcc.leagueMatchCached.MatchReporting.TeamIdsWithReportData)
-        {
-            Log.WriteLine("Loop on: " + teamKvp.Key + " with " + teamKvp.Value, LogLevel.VERBOSE);
-            if (teamKvp.Key == playerTeam.TeamId)
+            mcc = new MatchChannelComponents(_interfaceMessage);
+            if (mcc.interfaceLeagueCached == null || mcc.leagueMatchCached == null)
             {
-                Log.WriteLine("Found team: " + teamKvp.Key + " with name: " + teamKvp.Value.TeamName +
-                    " with playerId: " + playerId, LogLevel.VERBOSE);
+                Log.WriteLine(nameof(mcc) + " was null!", LogLevel.CRITICAL);
+                return Task.FromResult(new Response(nameof(mcc) + " was null!", false));
+            }
 
+            var playerId = _component.User.Id;
+            string[] splitStrings = thisInterfaceButton.ButtonCustomId.Split('_');
+            string playerSelectedPlane = splitStrings[0];
+
+            Log.WriteLine("Starting to find team with playerId: " + playerId +
+                " with selected plane: " + playerSelectedPlane, LogLevel.VERBOSE);
+
+            Team playerTeam = mcc.interfaceLeagueCached.LeagueData.FindActiveTeamByPlayerIdInAPredefinedLeagueByPlayerId(playerId);
+
+            Log.WriteLine("Finding with " + nameof(playerTeam) + ": " + playerTeam.TeamName + " with id: " + playerTeam.TeamId +
+                " on league: " + mcc.interfaceLeagueCached.LeagueCategoryName, LogLevel.VERBOSE);
+
+            foreach (var teamKvp in mcc.leagueMatchCached.MatchReporting.TeamIdsWithReportData)
+            {
                 try
                 {
-                    var planeReportObject =
-                        mcc.leagueMatchCached.MatchReporting.GetInterfaceReportingObjectWithTypeOfTheReportingObject(
-                            TypeOfTheReportingObject.PLAYERPLANE, playerTeam.TeamId) as PLAYERPLANE;
-
-                    if (planeReportObject == null)
+                    Log.WriteLine("Loop on: " + teamKvp.Key + " with " + teamKvp.Value, LogLevel.VERBOSE);
+                    if (teamKvp.Key == playerTeam.TeamId)
                     {
-                        Log.WriteLine(nameof(planeReportObject) + " was null!", LogLevel.CRITICAL);
-                        continue;
-                    }
+                        Log.WriteLine("Found team: " + teamKvp.Key + " with name: " + teamKvp.Value.TeamName +
+                            " with playerId: " + playerId, LogLevel.VERBOSE);
 
-                    Log.WriteLine(planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.Count.ToString(), LogLevel.DEBUG);
 
-                    if (!planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.ContainsKey(playerId))
-                    {
-                        Log.WriteLine("Does not contain: " + playerId, LogLevel.CRITICAL);
-                        continue;
-                    }
-                    else
-                    {
-                        Log.WriteLine("Contains: " + playerId, LogLevel.VERBOSE);
+                        var planeReportObject =
+                            mcc.leagueMatchCached.MatchReporting.GetInterfaceReportingObjectWithTypeOfTheReportingObject(
+                                TypeOfTheReportingObject.PLAYERPLANE, playerTeam.TeamId) as PLAYERPLANE;
 
-                        try
+                        if (planeReportObject == null)
                         {
+                            Log.WriteLine(nameof(planeReportObject) + " was null!", LogLevel.CRITICAL);
+                            continue;
+                        }
+
+                        Log.WriteLine(planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.Count.ToString(), LogLevel.DEBUG);
+
+                        if (!planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.ContainsKey(playerId))
+                        {
+                            Log.WriteLine("Does not contain: " + playerId, LogLevel.CRITICAL);
+                            continue;
+                        }
+                        else
+                        {
+                            Log.WriteLine("Contains: " + playerId, LogLevel.VERBOSE);
+
                             var playerIdSelectedPlane = planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.FirstOrDefault(
                                 x => x.Key == playerId);
 
@@ -105,27 +96,28 @@ public class PLANESELECTIONBUTTON : BaseButton
 
                             Log.WriteLine("Done modifying: " + playerId + " with plane: " +
                                 planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[playerId], LogLevel.DEBUG);
-                        }
-                        catch (Exception ex)
-                        {
-                            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
-                            continue;
+
                         }
 
+                        _interfaceMessage.GenerateAndModifyTheMessage();
+
+                        return Task.FromResult(new Response("", true));
                     }
-
-                    _interfaceMessage.GenerateAndModifyTheMessage();
-
-                    return Task.FromResult(new Response("", true));
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Log.WriteLine(ex.Message, LogLevel.CRITICAL);
                     continue;
                 }
             }
+            return Task.FromResult(new Response("Could not find: " + playerId, false));
+        }
+        catch (Exception ex)
+        {
+            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+            return Task.FromResult(new Response(ex.Message, false));
         }
 
-        return Task.FromResult(new Response("Could not find: " + playerId, false));
+
     }
 }
