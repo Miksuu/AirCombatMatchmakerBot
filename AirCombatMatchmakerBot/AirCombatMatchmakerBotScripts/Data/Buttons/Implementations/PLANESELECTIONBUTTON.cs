2e7019b93811df471685251a8faa7d51349e323e
Miksuu <imiksuu96@gmail.com>
2023-10-18 11:46:04 +0300

    First iteration of refactoring of PlaneSelectionButton's horrendous method with GPT

--- AirCombatMatchmakerBot/AirCombatMatchmakerBotScripts/Data/Buttons/Implementations/PLANESELECTIONBUTTON.cs
+++ AirCombatMatchmakerBot/AirCombatMatchmakerBotScripts/Data/Buttons/Implementations/PLANESELECTIONBUTTON.cs
@@ -23,135 +23,167 @@ public class PLANESELECTIONBUTTON : BaseButton
         return "";
     }
 
-    public override Task<Response> ActivateButtonFunction(
+    public override async Task<Response> ActivateButtonFunction(
         SocketMessageComponent _component, InterfaceMessage _interfaceMessage)
     {
         try
         {
-            mcc = new MatchChannelComponents(_interfaceMessage);
+            InitializeMatchComponents(_interfaceMessage);
             if (mcc.interfaceLeagueCached == null || mcc.leagueMatchCached == null)
             {
                 Log.WriteLine(nameof(mcc) + " was null!", LogLevel.ERROR);
-                return Task.FromResult(new Response(nameof(mcc) + " was null!", false));
+                return new Response(nameof(mcc) + " was null!", false);
             }
 
             var playerId = _component.User.Id;
-            string[] splitStrings = thisInterfaceButton.ButtonCustomId.Split('_');
-            string playerSelectedPlane = splitStrings[0];
+            string playerSelectedPlane = GetPlayerSelectedPlane();
 
-            Log.WriteLine("Starting to find team with playerId: " + playerId +
-                " with selected plane: " + playerSelectedPlane);
+            Log.WriteLine($"Starting to find team with playerId: {playerId} with selected plane: {playerSelectedPlane}");
 
-            Team playerTeam = mcc.interfaceLeagueCached.LeagueData.FindActiveTeamByPlayerIdInAPredefinedLeagueByPlayerId(playerId);
+            Team playerTeam = FindActiveTeamByPlayerId(playerId);
 
-            Log.WriteLine("Finding with " + nameof(playerTeam) + ": " + playerTeam.TeamName + " with id: " + playerTeam.TeamId +
-                " on league: " + mcc.interfaceLeagueCached.LeagueCategoryName);
+            Log.WriteLine($"Finding with {nameof(playerTeam)}: {playerTeam.TeamName} with id: {playerTeam.TeamId} on league: {mcc.interfaceLeagueCached.LeagueCategoryName}");
 
-            foreach (var teamKvp in mcc.leagueMatchCached.MatchReporting.TeamIdsWithReportData)
-            {
-                try
-                {
-                    Log.WriteLine("Loop on: " + teamKvp.Key + " with " + teamKvp.Value);
-                    if (teamKvp.Key != playerTeam.TeamId)
-                    {
-                        continue;
-                    }
-
-                    Log.WriteLine("Found team: " + teamKvp.Key + " with name: " + teamKvp.Value.TeamName +
-                        " with playerId: " + playerId);
-
-                    var planeReportObject =
-                        mcc.leagueMatchCached.MatchReporting.GetInterfaceReportingObjectWithTypeOfTheReportingObject(
-                            TypeOfTheReportingObject.PLAYERPLANE, playerTeam.TeamId) as PLAYERPLANE;
-                    if (planeReportObject == null)
-                    {
-                        Log.WriteLine(nameof(planeReportObject) + " was null!", LogLevel.ERROR);
-                        continue;
-                    }
-
-                    Log.WriteLine(planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.Count.ToString(), LogLevel.DEBUG);
-
-                    if (planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[playerId] != UnitName.NOTSELECTED)
-                    {
-                        return Task.FromResult(new Response("Already accepted: " + playerId, false));
-                    }
-
-                    if (!planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.ContainsKey(playerId))
-                    {
-                        Log.WriteLine("Does not contain: " + playerId, LogLevel.ERROR);
-                        continue;
-                    }
-
-                    Log.WriteLine("Contains: " + playerId);
-
-                    var playerIdSelectedPlane = planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.FirstOrDefault(
-                        x => x.Key == playerId);
-
-                    var unitNameInstance = (InterfaceUnit)EnumExtensions.GetInstance(playerSelectedPlane);
-
-                    Log.WriteLine("unitNameInstance:" + unitNameInstance);
-
-                    if (!planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.ContainsKey(playerId))
-                    {
-                        Log.WriteLine("does not contain", LogLevel.ERROR);
-                        continue;
-                    }
-
-                    planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[playerId] = unitNameInstance.UnitName;
-
-                    Log.WriteLine("Done modifying: " + playerId + " with plane: " +
-                        planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[playerId], LogLevel.DEBUG);
-
-                    // Check here that if everyone else is ready, skip the rest (unnecessary processing otherwise)
-                    bool skipCheck = false;
-                    foreach (var teamMember in mcc.leagueMatchCached.MatchReporting.TeamIdsWithReportData)
-                    {
-                        if (teamMember.Key != playerTeam.TeamId)
-                        {
-                            var otherTeamPlaneReportObject =
-                                mcc.leagueMatchCached.MatchReporting.GetInterfaceReportingObjectWithTypeOfTheReportingObject(
-                                    TypeOfTheReportingObject.PLAYERPLANE, teamMember.Key) as PLAYERPLANE;
-
-                            var status = otherTeamPlaneReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.First();
-
-                            if (status.Value != UnitName.NOTSELECTED)
-                            {
-                                skipCheck = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (skipCheck)
-                    {
-                        return Task.FromResult(new Response("", true));
-                    }
-
-                    // If timeUntil is more than 20minutes, add a temp event to cancel the queue after 5min
-                    var timeUntil = TimeService.CalculateTimeUntilWithUnixTime(
-                        mcc.leagueMatchCached.MatchEventManager.GetTimeUntilEventOfType(typeof(MatchQueueAcceptEvent)));
-                    if (timeUntil > 1200)
-                    {
-                        new TempQueueEvent(
-                            mcc.interfaceLeagueCached.LeagueCategoryId, mcc.leagueMatchCached.MatchChannelId,
-                            playerId, mcc.leagueMatchCached.MatchEventManager.ClassScheduledEvents);
-                    }
-
-                    _interfaceMessage.GenerateAndModifyTheMessage();
-
-                    return Task.FromResult(new Response("", true));
-                }
-                catch (Exception ex)
-                {
-                    Log.WriteLine(ex.Message, LogLevel.ERROR);
-                    continue;
-                }
-            }
-            return Task.FromResult(new Response("Could not find: " + playerId, false));
+            return await ProcessTeams(playerId, playerSelectedPlane, playerTeam, _interfaceMessage);
         }
         catch (Exception ex)
         {
             Log.WriteLine(ex.Message, LogLevel.ERROR);
-            return Task.FromResult(new Response(ex.Message, false));
+            return new Response(ex.Message, false);
         }
+    }
+
+    private void InitializeMatchComponents(InterfaceMessage _interfaceMessage)
+    {
+        mcc = new MatchChannelComponents(_interfaceMessage);
+    }
+
+    private string GetPlayerSelectedPlane()
+    {
+        string[] splitStrings = thisInterfaceButton.ButtonCustomId.Split('_');
+        return splitStrings[0];
+    }
+
+    private Team FindActiveTeamByPlayerId(ulong _playerId)
+    {
+        return mcc.interfaceLeagueCached.LeagueData.FindActiveTeamByPlayerIdInAPredefinedLeagueByPlayerId(_playerId);
+    }
+
+    private async Task<Response> ProcessTeams(
+        ulong _playerId, string _playerSelectedPlane, Team _playerTeam, InterfaceMessage _interfaceMessage)
+    {
+        foreach (var teamKvp in mcc.leagueMatchCached.MatchReporting.TeamIdsWithReportData)
+        {
+            try
+            {
+                Log.WriteLine($"Loop on: {teamKvp.Key} with {teamKvp.Value}");
+                if (teamKvp.Key != _playerTeam.TeamId)
+                {
+                    continue;
+                }
+
+                Log.WriteLine($"Found team: {teamKvp.Key} with name: {teamKvp.Value.TeamName} with playerId: {_playerId}");
+
+                var planeReportObject = GetPlaneReportObject(_playerTeam.TeamId);
+                if (planeReportObject == null)
+                {
+                    continue;
+                }
+
+                Log.WriteLine(planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.Count.ToString(), LogLevel.DEBUG);
+
+                if (planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[_playerId] != UnitName.NOTSELECTED)
+                {
+                    return new Response($"Already accepted: {_playerId}", false);
+                }
+
+                if (!planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.ContainsKey(_playerId))
+                {
+                    Log.WriteLine($"Does not contain: {_playerId}", LogLevel.ERROR);
+                    continue;
+                }
+
+                Log.WriteLine($"Contains: {_playerId}");
+
+                var unitNameInstance = GetUnitNameInstance(_playerSelectedPlane);
+
+                Log.WriteLine($"unitNameInstance: {unitNameInstance}");
+
+                if (!planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.ContainsKey(_playerId))
+                {
+                    Log.WriteLine("does not contain", LogLevel.ERROR);
+                    continue;
+                }
+
+                planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[_playerId] = unitNameInstance.UnitName;
+
+                Log.WriteLine($"Done modifying: {_playerId} with plane: {planeReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam[_playerId]}", LogLevel.DEBUG);
+
+                bool skipCheck = CheckIfEveryoneIsReady(_playerTeam.TeamId);
+                if (skipCheck)
+                {
+                    return new Response("", true);
+                }
+
+                var timeUntil = GetTimeUntil();
+                if (timeUntil > 1200)
+                {
+                    CreateTempQueueEvent(_playerId);
+                }
+
+                _interfaceMessage.GenerateAndModifyTheMessage();
+
+                return new Response("", true);
+            }
+            catch (Exception ex)
+            {
+                Log.WriteLine(ex.Message, LogLevel.ERROR);
+                continue;
+            }
+        }
+        return new Response($"Could not find: {_playerId}", false);
+    }
+
+    private PLAYERPLANE GetPlaneReportObject(int _teamId)
+    {
+        return mcc.leagueMatchCached.MatchReporting.GetInterfaceReportingObjectWithTypeOfTheReportingObject(
+            TypeOfTheReportingObject.PLAYERPLANE, _teamId) as PLAYERPLANE;
+    }
+
+    private InterfaceUnit GetUnitNameInstance(string _playerSelectedPlane)
+    {
+        return (InterfaceUnit)EnumExtensions.GetInstance(_playerSelectedPlane);
+    }
+
+    private bool CheckIfEveryoneIsReady(int _teamId)
+    {
+        foreach (var teamMember in mcc.leagueMatchCached.MatchReporting.TeamIdsWithReportData)
+        {
+            if (teamMember.Key != _teamId)
+            {
+                var otherTeamPlaneReportObject = GetPlaneReportObject(teamMember.Key);
+
+                var status = otherTeamPlaneReportObject.TeamMemberIdsWithSelectedPlanesByTheTeam.First();
+
+                if (status.Value != UnitName.NOTSELECTED)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private ulong GetTimeUntil()
+    {
+        return TimeService.CalculateTimeUntilWithUnixTime(
+            mcc.leagueMatchCached.MatchEventManager.GetTimeUntilEventOfType(typeof(MatchQueueAcceptEvent)));
+    }
+
+    private void CreateTempQueueEvent(ulong _playerId)
+    {
+        new TempQueueEvent(
+            mcc.interfaceLeagueCached.LeagueCategoryId, mcc.leagueMatchCached.MatchChannelId,
+            _playerId, mcc.leagueMatchCached.MatchEventManager.ClassScheduledEvents);
     }
 }
