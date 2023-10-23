846685ce054d4a1e5fea2ec93ec0b63e6db3e894
Miksuu <imiksuu96@gmail.com>
2023-10-23 15:52:16 +0300

    Fix Event handling fixing MatchScheduler and match events handling

---
+++ AirCombatMatchmakerBot/AirCombatMatchmakerBotScripts/DatabaseManagement/DatabaseComponents/EventScheduler/EventSchedulerComponents/EventTypeClasses/AdditionalEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+public static class AdditionalEvents
+{
+    public static ConcurrentBag<ScheduledEvent> GetAdditionalEvents()
+    {
+        ConcurrentBag<ScheduledEvent> events = new ConcurrentBag<ScheduledEvent>();
+
+        foreach (var league in Database.GetInstance<ApplicationDatabase>().Leagues.StoredLeagues)
+        {
+            foreach (var leagueEvent in league.GetLeagueEventsAndReturnThemInConcurrentBag())
+            {
+                events.Add(leagueEvent);
+            }
+        }
+
+        return events;
+    }
+}
