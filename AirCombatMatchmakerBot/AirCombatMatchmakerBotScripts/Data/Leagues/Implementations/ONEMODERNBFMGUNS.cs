d79ede9b88a38a8ca1426bfe0d943fc28d9d9660
Miksuu <imiksuu96@gmail.com>
2024-03-31 17:17:26 +0300

    Comment out the BFM guns league, causing the program not to compile

--- AirCombatMatchmakerBot/AirCombatMatchmakerBotScripts/Data/Leagues/Implementations/ONEMODERNBFMGUNS.cs
+++ AirCombatMatchmakerBot/AirCombatMatchmakerBotScripts/Data/Leagues/Implementations/ONEMODERNBFMGUNS.cs
@@ -1,29 +1,29 @@
-﻿using Discord;
-using Discord.WebSocket;
-using System.Runtime.Serialization;
-using System.Collections.Concurrent;
+﻿// using Discord;
+// using Discord.WebSocket;
+// using System.Runtime.Serialization;
+// using System.Collections.Concurrent;
 
-[DataContract]
-public class ONEMODERNBFMGUNS : BaseLeague
-{
-    public ONEMODERNBFMGUNS()
-    {
-        thisInterfaceLeague.LeagueCategoryName = LeagueName.ONEMODERNBFMGUNS;
-        thisInterfaceLeague.LeagueEra = Era.MODERN;
-        thisInterfaceLeague.LeaguePlayerCountPerTeam = 1;
+// [DataContract]
+// public class ONEMODERNBFMGUNS : BaseLeague
+// {
+//     public ONEMODERNBFMGUNS()
+//     {
+//         thisInterfaceLeague.LeagueCategoryName = LeagueName.ONEMODERNBFMGUNS;
+//         thisInterfaceLeague.LeagueEra = Era.MODERN;
+//         thisInterfaceLeague.LeaguePlayerCountPerTeam = 1;
 
-        thisInterfaceLeague.LeagueUnits = new ConcurrentBag<UnitName> {
-            UnitName.FA18C,
-            UnitName.F16C,
-            UnitName.M2000C,
-            UnitName.JF17,
-            UnitName.SU27,
-            UnitName.F14B,
-        };
-    }
+//         thisInterfaceLeague.LeagueUnits = new ConcurrentBag<UnitName> {
+//             UnitName.FA18C,
+//             UnitName.F16C,
+//             UnitName.M2000C,
+//             UnitName.JF17,
+//             UnitName.SU27,
+//             UnitName.F14B,
+//         };
+//     }
 
-    public override List<Overwrite> GetGuildPermissions(SocketGuild _guild, SocketRole _role)
-    {
-        return new List<Overwrite>();
-    }
-}
+//     public override List<Overwrite> GetGuildPermissions(SocketGuild _guild, SocketRole _role)
+//     {
+//         return new List<Overwrite>();
+//     }
+// }
